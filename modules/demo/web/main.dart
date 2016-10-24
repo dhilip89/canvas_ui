0c5f909e591944718393a360450e5aeb587c8910
Benjamin Strauß <b.strauss@netzbewegung.com>
2016-10-24 12:24:12 +0200

    update demo package config

--- modules/demo/web/main.dart
+++ modules/demo/web/main.dart
@@ -6,7 +6,7 @@ import 'dart:html' as html;
 void main() {
   html.CanvasElement stage = html.document.querySelector('#stage');
 
-  canvas_ui.setupCanvasUI(stage);
+  canvas_ui.CanvasUI canvasUI = new canvas_ui.CanvasUI(stage);
 
   runApp(new Center(child: new Text('Hello, world!')));
 }
